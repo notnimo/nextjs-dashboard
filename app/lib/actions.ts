f78b52a1b610fb8c847b1c5093a1399322afba06
not_nimo <damiano.livoni@gmail.com>
2025-10-15 13:19:07 +0100

    end (e)

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,6 +6,8 @@ import { redirect } from 'next/navigation';
 
 import postgres from 'postgres';
 import { id } from 'zod/v4/locales';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
  
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
  
@@ -119,4 +121,23 @@ export async function deleteInvoice(id: string) {
   }*/
   throw new Error('Failed to Delete Invoice');
   revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
